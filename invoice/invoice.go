82a572dcfe8f86870811c054a458d009dfb6c2c3
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2025-04-22 16:46:37 +0700

    refactor: public to internal getInvoiceSingature

--- invoice/invoice.go
+++ invoice/invoice.go
@@ -52,7 +52,7 @@ func (s *InvoiceService) Create(ctx context.Context, req CreateInvoiceRequest) (
 	headerParams := make(map[string]string)
 	headerParams[common.MerchantCodeHeader] = s.client.Cfg.MerchantCode
 	headerParams[common.TimeStampHeader] = s.client.GetCurrentTimestamp()
-	headerParams[common.SignatureHeader] = s.GenerateInvoiceSignature(headerParams[common.TimeStampHeader])
+	headerParams[common.SignatureHeader] = s.generateInvoiceSignature(headerParams[common.TimeStampHeader])
 
 	baseUrl := common.SandboxPOPBaseURL
 	if s.client.Cfg.Environment == common.ProductionEnv {
@@ -75,7 +75,7 @@ func (s *InvoiceService) Create(ctx context.Context, req CreateInvoiceRequest) (
 // It uses the API key from the config as the secret key to create the signature.
 // The result is returned as a hexadecimal-encoded string. This signature is used
 // for authenticating requests to the Duitku POP API.
-func (s *InvoiceService) GenerateInvoiceSignature(timestamp string) string {
+func (s *InvoiceService) generateInvoiceSignature(timestamp string) string {
 	h := hmac.New(sha256.New, []byte(s.client.Cfg.APIKey))
 	h.Write([]byte(s.client.Cfg.MerchantCode + timestamp))
 
