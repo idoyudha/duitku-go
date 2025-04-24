15392a12e6ceed974090dde1345d4a29311cb8cb
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2025-04-24 13:24:13 +0700

    docs: payment and transaction function docs

--- payment/payment.go
+++ payment/payment.go
@@ -21,6 +21,25 @@ func NewPaymentService(service *common.ServiceClient) *PaymentService {
 	}
 }
 
+// GetMethods gets the available payment methods for the given amount and datetime.
+// The method returns GetPaymentMethodResponse, HTTP response, and error.
+// The GetPaymentMethodResponse contains the available payment methods.
+//
+// The request body should match the GetPaymentMethodRequest struct.
+// The method will return error if the request body is empty or if the request
+// body is invalid.
+//
+// The method will also return error if the request to the server fails or if
+// the response from the server is invalid.
+//
+// The method will not return error if the request to the server is successful
+// and the response from the server is valid.
+//
+// The method will send a POST request to the server with the given request
+// body and headers.
+//
+// The method will use the sandbox base URL if the environment in the config
+// is set to sandbox. Otherwise, it will use the production base URL.
 func (s *PaymentService) GetMethods(ctx context.Context, req GetPaymentMethodRequest) (GetPaymentMethodResponse, *http.Response, error) {
 	res := &GetPaymentMethodResponse{}
 	path := "/merchant/paymentmethod/getpaymentmethod"
@@ -45,6 +64,10 @@ func (s *PaymentService) GetMethods(ctx context.Context, req GetPaymentMethodReq
 	return *res, httpRes, err
 }
 
+// generatePaymentSignature generates an HMAC SHA-256 signature for the given
+// parameter. It uses the API key from the config as the secret key to create
+// the signature. The result is returned as a hexadecimal-encoded string. This
+// signature is used for authenticating requests to the Duitku API.
 func (s *PaymentService) generatePaymentSignature(parameter string) string {
 	combinedStr := s.client.Cfg.MerchantCode + parameter + s.client.Cfg.APIKey
 	hash := sha256.Sum256([]byte(combinedStr))
--- transaction/transaction.go
+++ transaction/transaction.go
@@ -46,6 +46,25 @@ func (s *TransactionService) Create(ctx context.Context, req CreateTransactionRe
 	return *res, httpRes, err
 }
 
+// GetStatus gets the status of a transaction on duitku's server.
+// The method returns GetTransactionStatusResponse, HTTP response, and error.
+// The GetTransactionStatusResponse contains the status of the transaction.
+//
+// The request body should match the GetTransactionStatusRequest struct.
+// The method will return error if the request body is empty or if the request
+// body is invalid.
+//
+// The method will also return error if the request to the server fails or if
+// the response from the server is invalid.
+//
+// The method will not return error if the request to the server is successful
+// and the response from the server is valid.
+//
+// The method will send a POST request to the server with the given request
+// body and headers.
+//
+// The method will use the sandbox base URL if the environment in the config
+// is set to sandbox. Otherwise, it will use the production base URL.
 func (s *TransactionService) GetStatus(ctx context.Context, req GetTransactionStatusRequest) (GetTransactionStatusResponse, *http.Response, error) {
 	res := &GetTransactionStatusResponse{}
 	path := "/merchant/transactionStatus"
@@ -71,6 +90,11 @@ func (s *TransactionService) GetStatus(ctx context.Context, req GetTransactionSt
 	return *res, httpRes, err
 }
 
+// generateTransactionSignature generates an MD5 signature for the given
+// parameter. It uses the merchant code and API key from the config to
+// create a combined string, which is then hashed. The result is returned
+// as a hexadecimal-encoded string. This signature is used for authenticating
+// transaction requests to the Duitku API.
 func (s *TransactionService) generateTransactionSignature(parameter string) string {
 	combinedStr := s.client.Cfg.MerchantCode + parameter + s.client.Cfg.APIKey
 	hash := md5.Sum([]byte(combinedStr))
